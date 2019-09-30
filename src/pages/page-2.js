1d8211b9c9e7d03b017b2adac25f7bd3ffcced7c
Soumen Roy <soumen.r@ninestars.in>
2019-09-30 15:46:21 +0530

    page 3 added

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,6 +10,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <Link to="/page-3/">Go to page 3</Link>
   </Layout>
 )
 
